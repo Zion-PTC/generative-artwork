1764b67ab5ba5a51e5b7d77370dca692110d6cfd
gotek.tnl@gmail.com <>
2022-04-03 14:30:26 +0200

    Collection v1.0

--- Machines/GenerativeArtMachine/Collection.js
+++ Machines/GenerativeArtMachine/Collection.js
@@ -1,21 +1,87 @@
 import { System } from '../System/System.js';
-import { Class } from './Class.js';
+/**
+ * Folder structure types
+ * EDITION
+ * Ogni edizione è composta da elementi facenti parte
+ * della stessa rarità.
+ * Il dna manager conta quanti elementi sono presenti
+ * per ogni livello, in base alla rarità.
+ * 
+  I   Classe
+  II  └── Layers
+  III   └── Rarità
+  IV      └── Elementi
+ *
+ * La rarità è il III livello nella struttura
+ * delle directories.
+ * es :
+ * ci sono 3 livelli
+ * con 3 rarità:
+ * •comune: contenente 5 elementi
+ * •raro: contenente 2 elementi
+ * •super_raro: contente 1 elemento
+ * Il dna manager calcola tutte le combinazioni
+ * possibili fancendo
+ * n°elementiRarita1Livello1 = A = 5
+ * n°elementiRarita1Livello2 = B = 5
+ * n°elementiRarita1Livello3 = C = 5
+ * possibilitàComuni = A x B x C = 125
+ * n°elementiRarita2Livello1 = A = 2
+ * n°elementiRarita2Livello2 = B = 2
+ * n°elementiRarita2Livello3 = C = 2
+ * possibilitàComuni = A x B x C = 8
+ * n°elementiRarita1Livello1 = A = 1
+ * n°elementiRarita1Livello2 = B = 1
+ * n°elementiRarita1Livello3 = C = 1
+ * possibilitàComuni = A x B x C = 1
+ * totale = 125 + 8 + 1 = 134
+ * 
+ * ELEMENT
+ * Il dna Manager calcola tutte le possibili combinazioni
+ * di tutti gli elementi presenti
+ * elementiRari = 5 + 2 + 1 = 8
+ * ogni livello può avere 12 elementi
+ * Layer1Elementi = A = 8
+ * Layer2Elementi = B = 8
+ * Layer3Elementi = C = 8
+ * per calcolare le possibilità bisogna fare
+ * A x B x C = 512 Possibilità
+ * in queste possibilità tutti gli elementi compaiono
+ * con la stessa probabilità. Anche gli elementi rari
+ * compaiono nella stessa quantità ovvero ogni elemento
+ * appare 8 volte. Per far si che un elemento 
+ * 
+ * Ogni edizione può essere composta da elementi di
+ * rarità diversa.
+ */
 
 export class Collection {
   #count = 0;
+  #types = ['Edition', 'Element'];
+  #supply = 0;
+  #type;
   constructor(name, path, baseUri) {
     this.#count++;
     this.id = this.#count;
     this.name = name;
-    this.path = path;
-    this.baseUri = baseUri;
-    this.supply;
-    this.type;
+    this.path = path; // percorso della
+    // cartella che contiene i file e le
+    // cartelle della collezione
+    this.baseUri = baseUri; // percorso della
+    // cartella Collezioni della macchina che
+    // ha generato la collezione
     this.folderStructure = System.buildTree(this.path);
     this.classes = [];
   }
   //COLLECTIONS
-  static collectionExists(name) {}
+  static collectionExists(name, path) {
+    let elemetsInFolder = System.readDirSync(path);
+    let response;
+    elemetsInFolder.includes(name)
+      ? (response = true)
+      : (response = false);
+    return response;
+  }
   //CLASSES
   get arrayOfClasses() {
     let array = [];
@@ -45,15 +111,31 @@ export class Collection {
   }
   //LAYERS
   getCollections() {}
-  createNewCollection(name, classes) {
-    let newCollection = new Collection(
-      name,
-      this.path,
-      classes
-    );
-    this.collections.push(newCollection);
-    return newCollection;
-  }
   getReleasedEditions() {}
   getUnreleasedEditions() {}
+  // GETTERS
+  get supply() {
+    return this.#supply;
+  }
+  get type() {
+    return this.#type;
+  }
+  // SETTERS
+  /**
+   * @param {number} supply
+   */
+  set supply(supply) {
+    this.#supply = supply;
+    return this;
+  }
+  /**
+   * @param {number} type Indica il tipo di collezione :
+   * • 1 - Edition : la rarità degli elementi è basata sulla singola edizione
+   * • 2 - Element : la rarità delle edizioni è basata sulla rarità di ogni singolo elemento
+   */
+  set type(type) {
+    let typesIndex = type - 1;
+    this.#type = this.#types[typesIndex];
+    return this;
+  }
 }
--- test/GenerativeArtMachine/Collection.test.js
+++ test/GenerativeArtMachine/Collection.test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import Mocha from 'mocha';
 import { zionUtil } from '../../../telegram-bots/Classes/Utils.js';
 import { Collection } from '../../Machines/GenerativeArtMachine/Collection.js';
+import { System } from '../../Machines/System/System.js';
 
 const testRunner = new Mocha({ slow: 1000 });
 testRunner.suite.emit(
@@ -29,10 +30,13 @@ const _FOLDERSTRUCTURE = 'folderStructure';
 const _ROOT = 'root';
 
 let newCollection = new Collection(NAME, PATH, BASEURI);
+let arrayConIlContenutoDellaDirectory =
+  System.readDirSync(PATH);
+log(newCollection);
 
 export let CollectionTest =
   describe('COLLECTION CLASS', () => {
-    describe('Collection constructor', () => {
+    describe('COLLECTION CONSTRUCTOR', () => {
       it(`L'oggetto dovrebbe avere una proprieta: ${_ID}, con valore: 1`, () => {
         expect(newCollection[_ID]).to.be.equal(1);
       });
@@ -42,25 +46,69 @@ export let CollectionTest =
       it(`dovrebbe avere una proprietà: ${_PATH}, con valore ${PATH}`, () => {
         expect(newCollection[_PATH]).to.be.equal(PATH);
       });
-      it(`dovrebbe avere un paramentro ${_BASEURI}, con valore ${BASEURI}`, () => {
+      it(`dovrebbe avere un paramentro: ${_BASEURI}, con valore ${BASEURI}`, () => {
         expect(newCollection[_BASEURI]).to.be.equal(
           BASEURI
         );
       });
       describe(`Collection constructor attribute: ${_FOLDERSTRUCTURE}`, () => {
-        it(`dovrebbe avere una proprietà ${_FOLDERSTRUCTURE}, con valore: ${FOLDERSTRUCTURE}`, () => {
+        it(`dovrebbe avere una proprietà: ${_FOLDERSTRUCTURE}, con un valore`, () => {
           expect(newCollection[_FOLDERSTRUCTURE]).to.be.not
             .null;
         });
-
         it(`l'oggetto: ${_FOLDERSTRUCTURE}\ndovrebbe contenere un nodo con attributo: ${_ROOT},\ncon valore: '${PATH}'`, () => {
           expect(
             newCollection.folderStructure[_ROOT]
           ).to.be.equal(PATH);
         });
-        it(`l'oggetto: ${_FOLDERSTRUCTURE}\ndovrebbe contenere un child con attributo: ${_PATH},\ncon valore: '${PATH}/some'`, () => {
+        it(`l'oggetto: ${_FOLDERSTRUCTURE}\ndovrebbe contenere un child con attributo: ${_PATH},\ncon valore: '${arrayConIlContenutoDellaDirectory[0]}`, () => {
           expect(newCollection.folderStructure.children[0])
             .not.to.be.null;
+          expect(
+            newCollection.folderStructure.children[0].path
+          ).to.be.equal(
+            `${PATH}/${arrayConIlContenutoDellaDirectory[0]}`
+          );
+        });
+      });
+    });
+    describe(`COLLETION STATIC METHODS`, () => {
+      describe(`Method: collectionExists()`, () => {
+        it(`dovrebbe verificare che la directory della collezione: ${newCollection.name} non è ancora stata creata`, () => {
+          System.createNestedDir(BASEURI);
+          log(System.readDirSync(BASEURI));
+          expect(
+            Collection.collectionExists(NAME, BASEURI)
+          ).to.be.equal(false);
+          System.deleteRecursiveDir('./some');
+        });
+        it(`dovrebbe verificare che la directory della collezione: ${newCollection.name} è ancora stata creata`, () => {
+          System.createNestedDir(`${BASEURI}/${NAME}`);
+          log(System.readDirSync(BASEURI));
+          expect(
+            Collection.collectionExists(NAME, BASEURI)
+          ).to.be.equal(true);
+          System.deleteRecursiveDir('./some');
+        });
+      });
+    });
+    describe(`COLLECTION INSTANCE SETTERS`, () => {
+      const SUPPLY = 'supply';
+      const VALUE = 1000;
+      describe(`Method: set supply()`, () => {
+        it(`dovrebbe registrare l'attributo: ${SUPPLY} con un valore uguale a: ${VALUE}`, () => {
+          newCollection.supply = VALUE;
+          expect(newCollection.supply).to.be.equal(VALUE);
+        });
+      });
+      const TYPE = 'type';
+      const EXPECTEDVALUE = 'Edition';
+      describe(`Method: set type`, () => {
+        it(`dovrebbe registrare l'attributo: ${TYPE} con il valore: ${EXPECTEDVALUE}`, () => {
+          newCollection.type = 1;
+          expect(newCollection.type).to.be.equal(
+            EXPECTEDVALUE
+          );
         });
       });
     });
