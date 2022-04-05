e7ac1ed5de621121f363861aba30d59437c6684f
gotek.tnl@gmail.com <>
2022-04-05 17:16:03 +0200

    Element.js
    roba da nulla

--- Machines/GenerativeArtMachine/Element.js
+++ Machines/GenerativeArtMachine/Element.js
@@ -1,10 +1,17 @@
 export class Element {
-  constructor(name, path, collection, rarity, cl, layer) {
+  constructor(
+    name,
+    path,
+    collection,
+    rarity,
+    imageClass,
+    layer
+  ) {
     this.name = name;
     this.path = path;
     this.fileType;
     this.collection = collection;
-    this.class = cl;
+    this.imageClass = imageClass;
     this.layer = layer;
     this.rarity = rarity;
   }
