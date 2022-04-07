ac2c3398ae7c16fb4013fa194d10d0ab5a624cd3
gotek.tnl@gmail.com <>
2022-04-07 02:34:58 +0200

    sistemato rarity.js

--- Machines/GenerativeArtMachine/Rarity.js
+++ Machines/GenerativeArtMachine/Rarity.js
@@ -1,15 +1,53 @@
 import { System } from '../System/System.js';
+import { SystemEntity } from './SystemEntity.js';
 
-export class Rarity {
-  constructor(name, path, collection, cl, layer) {
-    this.name = name;
-    this.path = path;
-    this.collection = collection;
-    this.class = cl;
-    this.layer = layer;
-    this.elementCounts = 0;
-    this.percentage;
-    this.countElements();
+export class Rarity extends SystemEntity {
+  #classes = [];
+  #layers = [];
+  #elements = [];
+  constructor(
+    id,
+    name,
+    path,
+    level,
+    children,
+    type,
+    width,
+    height,
+    collectionName
+  ) {
+    super(
+      id,
+      name,
+      path,
+      level,
+      children,
+      type,
+      width,
+      height,
+      collectionName
+    );
+    this.from = from;
+    this.to = to;
+    this.percentage = percentage;
+  }
+  get classes() {
+    return this.#classes;
+  }
+  set classes(classes) {
+    return this.#classes.push(classes);
+  }
+  get layers() {
+    return this.#layers;
+  }
+  set layers(layers) {
+    return this.#layers.push(layers);
+  }
+  get elements() {
+    return this.#elements;
+  }
+  set elements(elements) {
+    return this.#elements.push(elements);
   }
   countElements() {
     return (this.elementCounts =
