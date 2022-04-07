dbe9608ab686cb33c81bad60861878ccd5facb52
gotek.tnl@gmail.com <>
2022-04-07 02:33:43 +0200

    creato CollectionDetails.js

---
+++ Machines/GenerativeArtMachine/CollectionDetails.js
@@ -0,0 +1,23 @@
+export class CollectionDetails {
+  #supply;
+  #type;
+  constructor(name, symbol, supply, baseURI, description) {
+    this.name = name;
+    this.symbol = symbol;
+    this.supply = supply;
+    this.baseURI = baseURI;
+    this.description = description;
+  }
+  get supply() {
+    return this.#supply;
+  }
+  set supply(supply) {
+    return (this.#supply = supply);
+  }
+  get type() {
+    return this.#type;
+  }
+  set type(type) {
+    this.#type = type;
+  }
+}
