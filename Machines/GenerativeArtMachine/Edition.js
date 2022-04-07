66b763738f3c9738708b5d981046281140e494e2
gotek.tnl@gmail.com <>
2022-04-07 15:44:15 +0200

    spostati metodi da Edition a user

--- Machines/GenerativeArtMachine/Edition.js
+++ Machines/GenerativeArtMachine/Edition.js
@@ -8,13 +8,4 @@ export class Edition {
     this.class;
     this.isEdited;
   }
-  static createEdition() {}
-  static createSpecificDNa() {}
-  static createNextEdition() {}
-  static createSpecificEdition() {}
-  static createEditions(from, to) {}
-  static getEdition(edition) {}
-  static getEditionsBatch() {}
-  static editCollection() {}
-  static searchCollection() {}
 }
---
+++ Machines/GenerativeArtMachine/User.js
@@ -0,0 +1,12 @@
+export class User {
+  constructor() {}
+  static createEdition() {}
+  static createSpecificDNa() {}
+  static createNextEdition() {}
+  static createSpecificEdition() {}
+  static createEditions(from, to) {}
+  static getEdition(edition) {}
+  static getEditionsBatch() {}
+  static editCollection() {}
+  static searchCollection() {}
+}
