b58247939bf3e83d5d4513a596b2415b83f60edb
gotek.tnl@gmail.com <>
2022-04-13 11:21:32 +0200

    Aggiunti getter e setters

--- Machines/GenerativeArtMachine/Nft_Metadata.js
+++ Machines/GenerativeArtMachine/Nft_Metadata.js
@@ -1,9 +1,50 @@
 export class Nft_Metadata {
-  constructor() {
-    this.name;
-    this.description;
-    this.image;
-    this.external_url;
-    this.attributes = [];
+  #name;
+  #description;
+  #image;
+  #external_url;
+  #attributes = [];
+  constructor(
+    name,
+    description,
+    image,
+    external_url,
+    attributes
+  ) {
+    this.#name = name;
+    this.#description = description;
+    this.#image = image;
+    this.#external_url = external_url;
+    this.#attributes = attributes;
+  }
+  get name() {
+    return this.#name;
+  }
+  get description() {
+    return this.#description;
+  }
+  get image() {
+    return this.#image;
+  }
+  get external_url() {
+    return this.#external_url;
+  }
+  get attributes() {
+    return this.#attributes;
+  }
+  set name(name) {
+    return (this.#name = name);
+  }
+  set description(description) {
+    return (this.#description = description);
+  }
+  set image(image) {
+    return (this.#image = image);
+  }
+  set external_url(external_url) {
+    return (this.#external_url = external_url);
+  }
+  set attributes(attributes = []) {
+    return this.#attributes.push([...attributes]);
   }
 }
