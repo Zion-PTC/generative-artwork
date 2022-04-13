c70816a289b9a909a279540b16d0ec332d37cf44
gotek.tnl@gmail.com <>
2022-04-13 11:37:32 +0200

    Sistemata Edition class

--- Machines/GenerativeArtMachine/Edition.js
+++ Machines/GenerativeArtMachine/Edition.js
@@ -1,11 +1,38 @@
-export class Edition {
-  constructor() {
-    this.id;
-    this.name;
-    this.type;
-    this.attributes;
-    this.description;
-    this.class;
-    this.isEdited;
+import { SystemEntity } from './SystemEntity';
+
+export class Edition extends SystemEntity {
+  #editions = [];
+  #imageFile;
+  constructor(
+    id,
+    name,
+    path,
+    level,
+    children = [],
+    type,
+    width,
+    height,
+    collection,
+    imageFile
+  ) {
+    super(
+      id,
+      name,
+      path,
+      level,
+      children,
+      type,
+      width,
+      height,
+      collection
+    );
+    this.#imageFile = imageFile;
+    this.#editions.push(this);
+  }
+  get imageFile() {
+    return this.#imageFile;
+  }
+  set imageFile(imageFile) {
+    return (this.#imageFile = imageFile);
   }
 }
