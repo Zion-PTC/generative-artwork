0dd05ff43866266b0a6661874e02b0f93e824381
gotek.tnl@gmail.com <>
2022-04-14 14:40:54 +0200

    implementati metodi

--- Machines/GeneratorMachine/GeneratorMachine.js
+++ Machines/GeneratorMachine/GeneratorMachine.js
@@ -5,6 +5,12 @@ export class GeneratorMachine {
   constructor() {}
   static Combinator = Combinator;
   static Picker = Picker;
-  static color() {}
-  static integerRandomNumber() {}
+  static color() {
+    let hue = Math.floor(Math.random() * 360);
+    let pastel = `hls(${hue}, 100%, 85%)`;
+    return pastel;
+  }
+  static integerRandomNumber(intervallo) {
+    return Math.floor(Math.random() * intervallo);
+  }
 }
