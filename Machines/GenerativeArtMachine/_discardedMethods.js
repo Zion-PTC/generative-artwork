c9b1cb1601c4261c4381c86591e2e7ed06cafc9a
gotek.tnl@gmail.com <>
2022-04-14 15:01:18 +0200

    Ordine

--- Machines/GenerativeArtMachine/_discardedMethods.js
+++ Machines/GenerativeArtMachine/_discardedMethods.js
@@ -227,4 +227,26 @@ export class _discardedMethods {
     });
     return array;
   }
+  haveAllLayersSameRarities(arrayOfArrays) {
+    let results = [];
+    for (
+      let index = 0;
+      index < arrayOfArrays.length - 1;
+      index++
+    ) {
+      const array = arrayOfArrays[index];
+      const nextArray = arrayOfArrays[index + 1];
+
+      if (zionUtil.checkArraysContent(array, nextArray)) {
+        return results.push(true);
+      } else {
+        return results.push(false);
+      }
+    }
+    if (!results.includes(false)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
