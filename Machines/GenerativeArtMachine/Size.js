01e5656010ddc4f83c46be90e3409832839ca7f1
gotek.tnl@gmail.com <>
2022-04-14 09:49:43 +0200

    Sistemato e creato test

--- Machines/GenerativeArtMachine/Size.js
+++ Machines/GenerativeArtMachine/Size.js
@@ -1,9 +1,20 @@
+import { zionUtil } from '../../../telegram-bots/Classes/Utils.js';
+
 export class Size {
+  static #sizes = [];
   #width;
   #height;
+  #ratio;
   constructor(width, height) {
     this.#width = width;
     this.#height = height;
+    this.#ratio = zionUtil.convertDecimalToFracionString(
+      this.#width / this.#height
+    );
+    Size.#sizes.push(this);
+  }
+  static get sizes() {
+    return this.#sizes;
   }
   get width() {
     return this.#width;
@@ -16,5 +27,8 @@ export class Size {
   }
   set height(height) {
     return (this.#height = height);
+  }
+  get ratio() {
+    return this.#ratio;
   }
 }
