eb5711e950f54949b7560eee348da4c98561a3d0
gotek.tnl@gmail.com <>
2022-04-19 09:42:34 +0200

    reinizializzata

--- Machines/GenerativeArtMachine/Edition.js
+++ Machines/GenerativeArtMachine/Edition.js
@@ -2,40 +2,8 @@ import { Dna } from './Dna';
 import { SystemEntity } from './SystemEntity';
 
 export class Edition extends SystemEntity {
-  #editions = [];
-  #imageFile;
-  #dna;
-  constructor(
-    id,
-    name,
-    path,
-    level,
-    children = [],
-    type,
-    width,
-    height,
-    collection,
-    imageFile
-  ) {
-    super(
-      id,
-      name,
-      path,
-      level,
-      children,
-      type,
-      width,
-      height,
-      collection
-    );
-    this.#imageFile = imageFile;
-    this.#editions.push(this);
-    this.#dna = new Dna();
-  }
-  get imageFile() {
-    return this.#imageFile;
-  }
-  set imageFile(imageFile) {
-    return (this.#imageFile = imageFile);
+  constructor(name, path, type, width, height, dna) {
+    super(name, path, type, width, height);
+    this.dna = new Dna(dna);
   }
 }
