7bb9bc8d780b0762dd4788c3ff6c460c99491180
Giacomo Gagliano <giacomo.gagliano@me.com>
2022-07-17 13:34:13 +0200

    v 0.0.139
    update

---
+++ built/src/ui/cli/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// import { Terminal } from "@zionstate/terminal";
+// const rl = Terminal.readline;
+// const itfc = rl.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false,
+// });
+// itfc.on("line", stop);
+// function stop(input: string) {
+//   const cond1 = input === "close";
+//   if (cond1) itfc.close();
+// }
---
+++ src/ui/cli/index.ts
@@ -0,0 +1,16 @@
+// import { Terminal } from "@zionstate/terminal";
+
+// const rl = Terminal.readline;
+
+// const itfc = rl.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+//   terminal: false,
+// });
+
+// itfc.on("line", stop);
+
+// function stop(input: string) {
+//   const cond1 = input === "close";
+//   if (cond1) itfc.close();
+// }
